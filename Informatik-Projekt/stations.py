c9694900458adfbd356c89bbe98451179bb73d9c
carl02a <carl.auchter02@icloud.com>
2025-12-30 12:33:59 +0100

    Add files via upload

---
+++ Informatik-Projekt/stations.py
@@ -0,0 +1,14 @@
+# stations.py 
+# Größere Bahnhöfe mit Koordinaten (Breite, Länge
+
+stations = {
+    "Basel Bad. Bf.": (47.5669, 7.6075),
+    "Freiburg Hbf": (47.9978, 7.8421),
+    "Offenburg": (48.4738, 7.9449),
+    "Baden-Baden": (48.7906, 8.1908),
+    "Karlsruhe Hbf": (49.0069, 8.4037),
+    "Mannheim Hbf": (49.4793, 8.4967),
+    "Frankfurt am Main Flughafen Fernbahnhof": (50.0528, 8.57),
+    "Köln Hbf": (50.9423, 6.9583)
+
+}
